9237f1603d4aa2c70beecafd0986bf1ad1135815
Pola17 <lucasdeaduriz@gmail.com>
2018-04-22 04:08:09 -0300

    v2

--- grilla.m
+++ grilla.m
@@ -1,17 +1,20 @@
 function resultado = grilla (parteEntera, parteDecimal, t)
   
-  #n = obtenerN (parteEntera, argn(2, :));
+  trimParteEntera = strtrim(argn(1,:));
+  trimParteDecimal = strtrim(argn(2,:));
   
-  n = obtenerN (quitarCeros(argn(1,:),"delanteros"), argn(2,:));
+  if (parteEntera > 0)
+    sinCerosAdelante = quitarCeros(trimParteEntera,"delanteros");
+    exp = length(sinCerosAdelante);
+    n = obtenerN (sinCerosAdelante, trimParteDecimal);
+  else
+    sinCerosAdelante = quitarCeros(trimParteDecimal,"delanteros");
+    exp = length(sinCerosAdelante) - length(trimParteDecimal);
+    n = sinCerosAdelante;
+  endif
   
   red = redondear (n ,t);
   
-  if (parteEntera > 0)
-    exp = length(num2str(parteEntera));
-  else
-    exp = length(num2str(parteDecimal)) - length(argn(2,:));
-  endif
-  
-  resultado = cstrcat("0.", quitarCeros(red, "todos"), " * 10^", num2str(exp));
+  resultado = cstrcat("0.", quitarCeros(red,"todos"), " * 10^", num2str(exp));
   
 endfunction
--- quitarCeros.m
+++ quitarCeros.m
@@ -3,18 +3,27 @@ function sinCeros = quitarCeros (numeroConCeros, condicion)
   i = 1;
   cerosInicio = 0;
   cerosFin = 0;
+  longitud = length(numeroConCeros);
   
-  while ((numeroConCeros(i) == '0') && (strcmp(condicion,"delanteros") == 1))
+  while ((numeroConCeros(i) == '0') && (strcmp(condicion,"delanteros") == 1 || strcmp(condicion,"todos") == 1))
     cerosInicio++;
+    if (longitud == cerosInicio)
+      sinCeros = "";
+      return;
+    endif;
     i++;  
   endwhile
   
-  i = length(numeroConCeros);
-  while ((numeroConCeros(i) == '0') && (strcmp(condicion,"delanteros") == 0))
+  i = longitud;
+  while ((numeroConCeros(i) == '0') && (strcmp(condicion,"todos") == 1))
     cerosFin++;
     i--;  
   endwhile
-
-  sinCeros = substr (numeroConCeros, 1+cerosInicio, length(numeroConCeros)-cerosFin-cerosInicio );  
+  
+#  if (x == cerosInicio)
+#    sinCeros = "0";
+#  else
+    sinCeros = substr (numeroConCeros, 1+cerosInicio, length(numeroConCeros)-cerosFin-cerosInicio );  
+#  endif
 
 endfunction
